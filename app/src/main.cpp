aec22674f54b0b2dff12a6c4097f36c15383cae6
Samanza Ahmed <samanzaahmed123@gmail.com>
2026-05-02 10:16:06 -0500

    done

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -4,7 +4,7 @@
 
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED_NODE DT_ALIAS(warning_led2)
+#define LED_NODE DT_ALIAS(app_led)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
 
@@ -23,7 +23,8 @@ int main(void)
 
         led_state = !led_state;
         LOG_INF("LED state: %s", led_state ? "ON" : "OFF");
-        k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        //k_msleep(CONFIG_BLINK_SLEEP_TIME_MS);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
